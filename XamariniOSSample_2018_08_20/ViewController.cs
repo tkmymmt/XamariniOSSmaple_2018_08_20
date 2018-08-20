ac004053f2db3c5a8eb572ce5c6293f8b535e504
takumi yamamoto <takumi.yamamoto@frog-pod.com>
2018-08-20 18:33:57 +0900

    Animation、TapRecognizerテスト

--- XamariniOSSample_2018_08_20/ViewController.cs
+++ XamariniOSSample_2018_08_20/ViewController.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using CoreAnimation;
 using UIKit;
 
 namespace XamariniOSSample_2018_08_20
@@ -14,7 +14,48 @@ namespace XamariniOSSample_2018_08_20
 		public override void ViewDidLoad()
 		{
 			base.ViewDidLoad();
-			// Perform any additional setup after loading the view, typically from a nib.
+
+			var view = new UIView
+			{
+				TranslatesAutoresizingMaskIntoConstraints = false,
+				BackgroundColor = UIColor.Blue
+			};
+
+			UITapGestureRecognizer tapGestureRecognizer = new UITapGestureRecognizer(() =>
+			{
+				UIView.Transition(view, 1.0, UIViewAnimationOptions.TransitionCurlDown, null /*() => view.BackgroundColor = (view.BackgroundColor == UIColor.Blue) ? UIColor.Red : UIColor.Blue*/, null);
+				UIView.Animate(0.1, () => view.BackgroundColor = (view.BackgroundColor == UIColor.Blue) ? UIColor.Red : UIColor.Blue, null);
+			});
+			view.AddGestureRecognizer(tapGestureRecognizer);
+
+			View.AddSubview(view);
+
+
+			UISwipeGestureRecognizer rightSwipe = new UISwipeGestureRecognizer(
+				() => UIView.Transition(view, 1.0, UIViewAnimationOptions.TransitionFlipFromLeft, null, null));
+			rightSwipe.Direction = UISwipeGestureRecognizerDirection.Right;
+			View.AddGestureRecognizer(rightSwipe);
+
+			UISwipeGestureRecognizer leftSwipe = new UISwipeGestureRecognizer(
+				() => UIView.Transition(view, 1.0, UIViewAnimationOptions.TransitionFlipFromRight, null, null));
+			leftSwipe.Direction = UISwipeGestureRecognizerDirection.Left;
+			View.AddGestureRecognizer(leftSwipe);
+
+			UISwipeGestureRecognizer upSwipe = new UISwipeGestureRecognizer(
+				() => UIView.Transition(view, 1.0, UIViewAnimationOptions.TransitionFlipFromTop, null, null));
+			upSwipe.Direction = UISwipeGestureRecognizerDirection.Up;
+			View.AddGestureRecognizer(upSwipe);
+
+			UISwipeGestureRecognizer downSwipe = new UISwipeGestureRecognizer(
+				() => UIView.Transition(view, 1.0, UIViewAnimationOptions.TransitionFlipFromBottom, null, null));
+			downSwipe.Direction = UISwipeGestureRecognizerDirection.Down;
+			View.AddGestureRecognizer(downSwipe);
+
+
+			view.LeftAnchor.ConstraintEqualTo(View.LeftAnchor, 100).Active = true;
+			view.RightAnchor.ConstraintEqualTo(View.RightAnchor, -100).Active = true;
+			view.HeightAnchor.ConstraintEqualTo(view.WidthAnchor).Active = true;
+			view.CenterYAnchor.ConstraintEqualTo(View.CenterYAnchor).Active = true;
 		}
 
 		public override void DidReceiveMemoryWarning()
